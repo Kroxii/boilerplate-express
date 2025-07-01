b7a668a9f3b28615f78e6dccc572dcdad60663cc
Kroxii <kilona.soleis@gmail.com>
2025-07-01 13:49:36 +0200

    step 6

--- myApp.js
+++ myApp.js
@@ -1,5 +1,6 @@
-let express = require('express');
-let app = express();
+var express = require('express');
+var app = express();
+require('dotenv').config();
 
 console.log("Hello World");
 
@@ -10,10 +11,13 @@ app.get("/", function(req, res) {
 app.use("/public", express.static(__dirname + "/public"));
 
 
-app.get("/json", (req, res) => {
-  res.json({
-    message: "Hello json"
-  });
-});
+
+app.get("/json", function(req, res) {
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    res.json({ "message": "HELLO JSON" });
+  } else {
+    res.json({ "message": "Hello json" });
+  }
+})
 
  module.exports = app;
