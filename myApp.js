6f3e8897f619592cf81132315daa0618b07b2a5b
Kroxii <kilona.soleis@gmail.com>
2025-07-01 10:06:47 +0200

    step 5

--- myApp.js
+++ myApp.js
@@ -9,4 +9,11 @@ app.get("/", function(req, res) {
 
 app.use("/public", express.static(__dirname + "/public"));
 
+
+app.get("/json", (req, res) => {
+  res.json({
+    message: "Hello json"
+  });
+});
+
  module.exports = app;
